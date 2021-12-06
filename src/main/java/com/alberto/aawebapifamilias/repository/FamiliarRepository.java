9c687c600a544b3efb32cc35e8e46d5e2cf6be93
albertrodtab <albertrodtab@gmail.com>
2021-12-07 00:29:53 +0100

    Añado funcionalidad al Controller de familiar, se puede añadir, modificar, eliminar y consultar objetos de la clase familiar

--- src/main/java/com/alberto/aawebapifamilias/repository/FamiliarRepository.java
+++ src/main/java/com/alberto/aawebapifamilias/repository/FamiliarRepository.java
@@ -4,7 +4,14 @@ import com.alberto.aawebapifamilias.domain.Familiar;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FamiliarRepository extends CrudRepository<Familiar, Long> {
 
+    Familiar findAllById(long id);
+    List<Familiar> findAll();
+    List<Familiar> findAllFamiliaresById(long id);
+
 }
+
